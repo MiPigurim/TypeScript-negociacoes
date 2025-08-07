75b978c3101b1a9dc6334b86b48ab2014447685b
Michele Pigurim <michelepigurim26@gmail.com>
2025-08-07 15:09:07 -0300

    refactor para debug

---
+++ app/dist/js/models/negociacaoes.js
@@ -0,0 +1,18 @@
+export class Negociacoes {
+    constructor() {
+        this.negociacoes = [];
+    }
+    adiciona(negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+    lista() {
+        return this.negociacoes;
+    }
+    paraTexto() {
+        return JSON.stringify(this.negociacoes, null, 2);
+    }
+    ehIgual(negociacaoes) {
+        return (JSON.stringify(this.negociacoes) === JSON.stringify(negociacaoes.lista()));
+    }
+}
+//# sourceMappingURL=negociacaoes.js.map
